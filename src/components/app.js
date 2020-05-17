3b7143cb785c0f25437a5dfb953a24c298aacf94
Stephon Lawson <slawso12@gmail.com>
2020-05-17 13:22:16 -0700

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react';
+import SearchBar from '../components/SearchBar';
+
+const App = () => {
+    return (
+        <>
+        <div>
+            <SearchBar />
+        </div>
+        </>
+    );
+};
+
+
+
+export default App; 
+
