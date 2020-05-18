717edef7e17a44924e559451c98058947e9a0ff3
Stephon Lawson <slawso12@gmail.com>
2020-05-17 17:33:30 -0700

    Update: Event-Handling is implemented

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+    
+    onFormSubmit = (event) => {
+        event.preventDefault(); // prevent default behavior that would cause the page to refresh itself
+
+        this.props.onSubmit(this.state.term);
+        
+    }
+    
+
     render() {
         return (
-            <div className="ui segment"> 
-            <form className="ui form">
-                <div className="field">
-                    <label>Image Search</label>
-                 <input type="text" placeholder="search"/>   
-                </div>
-            </form>
-        </div>
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" 
+                        placeholder="search" 
+                        value={this.state.term}
+                        onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })} />
+                    </div>
+                </form>
+            </div>
         );
     };
 };
--- src/components/app.js
+++ src/components/app.js
@@ -2,17 +2,26 @@ import React from 'react';
 import ReactDOM from 'react';
 import SearchBar from '../components/SearchBar';
 
-const App = () => {
-    return (
-        <>
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar />
-        </div>
-        </>
-    );
-};
+class App extends React.Component {
+
+    onSearchSubmit = (term) => {
+        console.log(term);
+
+    };
+
+    render() {
+        return (
+            <>
+                <div className="ui container" style={{ marginTop: '10px' }}>
+                    <SearchBar onSubmit={this.onSearchSubmit}/>
+                </div>
+            </>
+        );
+    };
+}
 
 
 
-export default App; 
+
+export default App;
 
