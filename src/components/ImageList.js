fdb70c218ef667a43fcf52db5b42b5124a92e17f
Stephon Lawson <slawso12@gmail.com>
2020-05-19 21:10:36 -0700

    Refactor: grid implemented

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
  
 const ImageList = (props) => {  //receive images from the parent
    const images = props.images.map((image) => {
-        return <img key={image.id} alt={image.description} src={image.urls.regular}/>
+        return <ImageCard key={image.id} image={image}/>
    })
     
-return <div>{images}</div>
+return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
